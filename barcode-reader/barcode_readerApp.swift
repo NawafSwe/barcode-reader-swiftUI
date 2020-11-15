160268990e5db98e54f070c31128c79780d921a2
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-15 17:28:58 +0300

    Initial Commit

---
+++ barcode-reader/barcode_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_readerApp.swift
+//  barcode-reader
+//
+//  Created by Nawaf B Al sharqi on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct barcode_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
